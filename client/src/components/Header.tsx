d1a5f56ef15cca133e100f69d980acccf76c68e3
Rithish <bajjuririthish521@gmail.com>
2025-11-12 10:57:42 +0530

    Adjust logo image height in Header component

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -77,7 +77,7 @@ export default function Header() {
               <img 
                 src={logoImage} 
                 alt="ANVZ Logo" 
-                className="h-12.5 w-auto dark:invert"
+                className="h-10 w-auto dark:invert"
               />
             </div>
             
